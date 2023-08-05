b4e5e3a7fdb6dedce0502e17ce7f99590336edcc
Roggers Anguzu <141458053+RoggersAnguzu@users.noreply.github.com>
2023-08-05 17:08:20 +0300

    Add files via upload
    A simple Fibonacci generator

---
+++ FIBONACInIUMBERS/FIBONACInIUMBERS/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace FIBONACI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MasterCaller();
+        }
+        public static int Recurse(int fibobacci)
+        {
+            if(fibobacci<=1)
+            {
+                return fibobacci;
+            }
+            else
+            {
+                return (fibobacci - 1) + (fibobacci - 2);
+            }
+        }
+        public static  void MasterCaller()
+        {
+            Console.WriteLine("Kindly Enter the range of the fibonaci numbers to be entered: ");
+            int Fib = int.Parse(Console.ReadLine());
+
+            for(int i =0;i<Fib;i++)
+            {
+                Console.WriteLine(Recurse(i));
+            }
+            Console.WriteLine("Congratulations Your numbers are finished: ");
+        }
+    }
+}
